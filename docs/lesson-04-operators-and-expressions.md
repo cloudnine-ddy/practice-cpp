f0671cb9477c9ea5c464c2562ac1734d59c2fa3d
jtii0004 <jtii0004@student.monash.edu>
2026-06-02 20:52:43 +0800

    lesson(04): operators and expressions

---
+++ docs/lesson-04-operators-and-expressions.md
@@ -0,0 +1,24 @@
+# Lesson 04 - Operators and Expressions
+
+## What I learned
+
+- Some Syntax of C++:
+
+    - `+` -> addition operator  
+    
+    - `-` -> subtraction operator  
+    
+    - `*` -> multiplication operator  
+    
+    - `/` -> division operator  
+        - if both numbers are integers,it ONLY keep the integer part of the result, without rouding
+        
+        - if any of the numbers is a floating-point number, it will be normal division (floating-point division)
+    
+    - `%` -> modulus operator (remainder of division)  
+    
+    - `=` -> assignment operator
+
+## Result
+
+- NIL
---
+++ lessons/04_operators/main.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+
+int main()
+{
+    double width;
+    double height;
+
+    std::cout << "Enter rectangle width: ";
+    std::cin >> width;
+
+    std::cout << "Enter rectangle height: ";
+    std::cin >> height;
+
+    double area = width * height;
+    double perimeter = 2 * (width + height);
+
+    std::cout << std::endl;
+    std::cout << "Rectangle Result" << std::endl;
+    std::cout << "----------------" << std::endl;
+    std::cout << "Area: " << area << std::endl;
+    std::cout << "Perimeter: " << perimeter << std::endl;
+
+    int totalMinutes;
+
+    std::cout << std::endl;
+    std::cout << "Enter total minutes: ";
+    std::cin >> totalMinutes;
+
+    int hours = totalMinutes / 60;
+    int minutes = totalMinutes % 60;
+
+    std::cout << totalMinutes << " minutes = ";
+    std::cout << hours << " hour(s) and ";
+    std::cout << minutes << " minute(s)" << std::endl;
+
+    int number;
+
+    std::cout << std::endl;
+    std::cout << "Enter an integer: ";
+    std::cin >> number;
+
+    int remainder = number % 2;
+
+    std::cout << number << " % 2 = " << remainder << std::endl;
+
+    return 0;
+}
