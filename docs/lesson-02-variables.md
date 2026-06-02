4514cf3e790b1b90c43f1fa1df78b53c2865102d
jtii0004 <jtii0004@student.monash.edu>
2026-06-02 13:44:55 +0800

    lesson(02): learned basic datatype, their syntax, and variable concept

---
+++ docs/lesson-02-variables.md
@@ -0,0 +1,22 @@
+# Lesson 02 - Variables
+
+## What I learned
+
+- Some Syntax of C++:
+
+    - `int age = 21;` 
+    
+    - `double height = 1.67;`
+    
+    - `char grade = 'HD';`  
+    note that char uses a single quote `' '`
+
+    - `bool isStudent = true;`
+    
+    - `std::string name = "Jorden";`  
+     note that char uses a single quote `" "`
+
+
+## Result
+
+- NIL
---
+++ lessons/02_variables/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+    int age = 21;
+    double height = 1.67;
+    char grade = 'HD';
+    bool isStudent = true;
+    std::string name = "Jorden";
+
+    std::cout << "Name: " << name << std::endl;
+    std::cout << "Age: " << age << std::endl;
+    std::cout << "Height: " << height << std::endl;
+    std::cout << "Grade: " << grade << std::endl;
+    std::cout << "Is student: " << isStudent << std::endl;
+
+    return 0;
+}
